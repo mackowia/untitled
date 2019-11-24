3ae184e3d98049da31ea91df1cf7fcf833bbad7d
Magda <mmackowiak.16@o2.pl>
2019-11-24 14:26:16 +0100

    nowy_katalog_zjazd_II/dzień_2

---
+++ zjazd_II/dzień_2/ex_3_1_1.py
@@ -0,0 +1,13 @@
+#bez zagniezdzenia
+def policz_znaki ...:
+
+def test_pojedyncze_znaki_domyslne():
+    assert policz_znaki ('ala ma <kota> a kot ma ale') == 4
+
+# def test_pojedyncze_znaki_domyslne():
+#     assert policz_znaki ('ala [kota [a kot]] ma [ale]', '[', ']') == 18
+#
+# def test_pojedyncze_znaki_domyslne():
+#    assert policz_znaki ('a <a<a<a>>>') == 6
+
+
